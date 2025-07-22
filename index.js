fd377f0cf36bb38b8ca2d284f5bfbfe56ec59007
sazzad731 <sazzadho731@gmail.com>
2025-07-22 10:43:17 +0600

    Created meal details api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors")
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors())
 app.use(express.json());
@@ -30,7 +30,7 @@ async function run() {
     const mealsCollection = db.collection("meals")
 
 
-
+    // get meals by category
     app.get('/meals', async(req, res)=>{
       const category = req.query.category;
       let query = {}
@@ -39,6 +39,15 @@ async function run() {
       }
       const result = await mealsCollection.find(query).limit(3).toArray();
       res.send(result)
+    })
+
+
+    // get Meal details
+    app.get('/meal/:id', async(req, res)=>{
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await mealsCollection.findOne(query);
+      res.send(result);
     })
 
 
