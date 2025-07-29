d417dce281023131300caea4bdf17352bc136527
sazzad731 <sazzadho731@gmail.com>
2025-07-29 23:39:02 +0600

    Added vercel.json

--- index.js
+++ index.js
@@ -307,18 +307,22 @@ async function run() {
 
 
 
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res)=>{
+  res.send("Server running")
+})
 
 
 
